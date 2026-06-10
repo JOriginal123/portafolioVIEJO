512cfc9a8c2e031feb381bc8f3925d5e9d1282ed
JOriginal123 <britosjoaquin61@gmail.com>
2026-06-10 18:46:18 -0300

    Modificacion 4

---
+++ Practica/script.js
@@ -0,0 +1,13 @@
+function guardar(){
+    let nombre= document.getElementById("nombre").value;
+    let apellido= document.getElementById("apellido").value;
+    let edad= document.getElementById("edad").value;
+    let email= document.getElementById("email").value;
+    let fecha_nacimiento= document.getElementById("fecha-nacimiento").value;
+    let nombre= document.getElementById("generoM").value;
+    let nombre= document.getElementById("generoF").value;    
+    
+    alert(nombre);
+    document.write(apellido)
+    document.getElementById("mensaje").innerHTML=apellido
+}
